64bc8d8fd1f9da11901584fb087566db14ee442d
Keshava Varma N S <keshavavarmans@Keshava-Verma-N-S-s-MacBook-Air.local>
2022-01-21 18:06:07 +0530

    changed types

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,27 +1,42 @@
-type Props = {
-    question:string;
-    answers:string[];
-    callback:any;
-    userAnswer:any;
-    questionNr:number;
-    totalQuestions:number;
-}
+import { AnswerObject } from "../App";
 
-const QuestionCard = ({question,answers,callback,userAnswer,questionNr,totalQuestions}:Props)=>{
-    return (<div>
-        <p className="number">
-            Question:{questionNr}/{totalQuestions}
-        </p>
-        <p dangerouslySetInnerHTML={{__html:question}}/>
-        <div>
-            {answers.map(answer=>(
-                <div key={answer}>
-                    <button disabled={userAnswer} value={answer} onClick={callback}>
-                        <span dangerouslySetInnerHTML={{__html:answer}}/>
-                    </button>
-                </div>
-            ))}
-        </div>
-    </div>)
-}
-export default QuestionCard
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}: Props) => {
+  return (
+    <div>
+      <p className="number">
+        Question:{questionNr}/{totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <div key={answer}>
+            <button
+              disabled={userAnswer ? true : false}
+              value={answer}
+              onClick={callback}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default QuestionCard;
