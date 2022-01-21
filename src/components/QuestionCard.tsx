05ca72b1d5734f1c5ff2e79e3f312a1edf0c95af
Keshava Varma N S <keshavavarmans@Keshava-Verma-N-S-s-MacBook-Air.local>
2022-01-21 17:59:55 +0530

    quiz functionality done

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,27 @@
+type Props = {
+    question:string;
+    answers:string[];
+    callback:any;
+    userAnswer:any;
+    questionNr:number;
+    totalQuestions:number;
+}
+
+const QuestionCard = ({question,answers,callback,userAnswer,questionNr,totalQuestions}:Props)=>{
+    return (<div>
+        <p className="number">
+            Question:{questionNr}/{totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{__html:question}}/>
+        <div>
+            {answers.map(answer=>(
+                <div key={answer}>
+                    <button disabled={userAnswer} value={answer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{__html:answer}}/>
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>)
+}
+export default QuestionCard
